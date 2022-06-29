aa8a29ab03c2adac72c31c8fa6d1279ac32f4def
Nikhil <nikhil.c@credright.com>
2022-06-29 18:43:16 +0530

    inner page design

---
+++ app/src/main/java/com/lnd/salon/ui/YourAppointmentFragment.kt
@@ -0,0 +1,25 @@
+package com.lnd.salon.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.lnd.salon.R
+import com.lnd.salon.databinding.ActivityMainBinding
+import com.lnd.salon.databinding.FragmentYourAppointmentBinding
+
+
+class YourAppointmentFragment : Fragment() {
+
+   lateinit var binding:FragmentYourAppointmentBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentYourAppointmentBinding.inflate(layoutInflater,container,false)
+        return binding.root
+    }
+
+
+}
